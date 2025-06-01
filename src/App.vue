1cf69700b6180626a9572f4f4bd86ccf65def35d
ssong <ssong@ssong.com>
2025-06-01 14:01:50 +0900

    부트스트랩 적용

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <router-link to="/">홈</router-link>
+    <br>
     <router-link to="/about">소개</router-link>
     <router-view></router-view>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>Home</div>
+    <div>
+        <button class="btn btn-primary">Primary 버튼</button>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                드롭다운
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">메뉴 1</a></li>
+                <li><a class="dropdown-item" href="#">메뉴 2</a></li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script setup>
