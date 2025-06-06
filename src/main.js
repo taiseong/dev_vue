72177dd39af07309a8b6a4e28d1dbb0ab0652865
ssong <ssong>
2025-06-06 17:02:09 +0900

    메인,레프트 템플릿

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import router from './router'
 
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap'
 //import '@/assets/css/bootstrap.min.css'
 
 //import '@/assets/js/plugin/webfont/webfont.min.js'
 
-import '@/assets/css/plugins.css'
-import '@/assets/css/kaiadmin.min.css'
+//import '@/assets/css/plugins.css'
+//import '@/assets/css/kaiadmin.min.css'
 
 createApp(App).use(router).mount('#app')
