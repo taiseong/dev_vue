465e34ae45f3bf7c24e5614c795385cc4006252d
ssong <ssong>
2025-06-07 16:34:58 +0900

    pania 추가

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
+import 'bootstrap'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap'
+import '@/assets/css/main.css'   // <-- 전역 스타일 import
+
 //import '@/assets/css/bootstrap.min.css'
 
 //import '@/assets/js/plugin/webfont/webfont.min.js'
@@ -14,4 +18,7 @@ import 'bootstrap'
 //import '@/assets/css/plugins.css'
 //import '@/assets/css/kaiadmin.min.css'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+createApp(App).use(pinia).use(router).mount('#app')
