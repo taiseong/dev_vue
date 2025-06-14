f2e762703be42c49da85a7bb7721962f496f41b6
ssong22 <ssong22>
2025-06-14 15:57:12 +0900

    git test2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -304,7 +304,7 @@ const fetchPosts = async () => {
 
 function moveDetail(postId) {
     // 상세 페이지 이동 로직
-    //alert('상세 페이지 이동: ' + postId111111)
+    //alert('상세 페이지 이동: ' + postId111111222)
     router.push({ name: 'postDetail', params: { postId } })
 }
 
